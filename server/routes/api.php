a88f19748ad81305d0a20aa2d72a94d212f5f8fc
BenniesM <benadegbaju@gmail.com>
2019-10-09 10:58:46 +0100

    Added server configuration for API endpoints: * Laravel framework 6.0 * Configured server for handling api requests * Configured endpoints for user registration, login and logout requests.

---
+++ server/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('register', 'Auth\RegisterController@register');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::group(['middleware' => ['auth:api', 'cors']], function() {
+
+});
